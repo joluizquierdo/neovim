224a8b7cf3934e8d38ecb7cf7ec18e16a73c4831
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2026-02-09 19:34:10 +0100

    feat: add treesitter indentiation bydefault in each file

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   build = ':TSUpdate',
   config = function()
-      -- Install parsers, check list of parsers here: 
+      -- Install parsers, check list here: 
       -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
       local treesitter = require('nvim-treesitter')
       treesitter.install({
@@ -22,6 +22,16 @@ return {
           'yaml',
           'vim',
           'vimdoc',
+      })
+      -- Enable TreeSitter indentation globally
+      vim.api.nvim_create_autocmd('FileType', {
+          pattern = '*',
+          callback = function()
+              -- Enable TreeSitter-based indentation if parser is available
+              if pcall(vim.treesitter.get_parser) then
+                  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+              end
+          end,
       })
   end,
 }
