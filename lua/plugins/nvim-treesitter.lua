6ab668c895c1832aa0c268b742bcf5ac03d0c33d
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2026-02-11 14:09:47 +0100

    style: fix indentation for nvim-treesitter.lua

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,38 +1,43 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate',
-  config = function()
-      -- Install parsers, check list here: 
-      -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
-      local treesitter = require('nvim-treesitter')
-      treesitter.install({
-          'lua',
-          'python',
-          'go',
-          'rust',
-          'typescript',
-          'javascript',
-          'bash',
-          'zsh',
-          'html',
-          'helm',
-          'terraform',
-          'dockerfile',
-          'json',
-          'yaml',
-          'vim',
-          'vimdoc',
-      })
-      -- Enable TreeSitter indentation globally
-      vim.api.nvim_create_autocmd('FileType', {
-          pattern = '*',
-          callback = function()
-              -- Enable TreeSitter-based indentation if parser is available
-              if pcall(vim.treesitter.get_parser) then
-                  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-              end
-          end,
-      })
-  end,
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+        -- Install parsers, check list here: 
+        -- https://github.com/nvim-treesitter/nvim-treesitter/blob/main/SUPPORTED_LANGUAGES.md
+        local treesitter = require('nvim-treesitter')
+        treesitter.install({
+            'lua',
+            'python',
+            'go',
+            'rust',
+            'typescript',
+            'javascript',
+            'bash',
+            'zsh',
+            'html',
+            'helm',
+            'terraform',
+            'dockerfile',
+            'json',
+            'yaml',
+            'vim',
+            'vimdoc',
+        })
+
+        -- Load treesitter, without this call some things 
+        -- like folding will not work
+        treesitter.setup()
+
+        -- Enable TreeSitter indentation globally
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = '*',
+            callback = function()
+                -- Enable TreeSitter-based indentation if parser is available
+                if pcall(vim.treesitter.get_parser) then
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end,
+        })
+    end,
 }
