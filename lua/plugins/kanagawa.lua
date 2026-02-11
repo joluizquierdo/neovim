9e2a7ae4e9536ba6448dbfdfd55060a91b48ac4d
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2026-02-11 14:08:12 +0100

    refactor: change colorscheme back to kanagawa

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -4,7 +4,7 @@ return {
     priority = 1001, -- Load before other plugins
     config = function()
         require('kanagawa').setup({}) -- default settings
-        vim.cmd('colorscheme kanagawa-dragon')
+        vim.cmd('colorscheme kanagawa')
 
         -- Override spell highlighting for better visibility
         -- Uses underline + color which works in most terminals
