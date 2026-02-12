401bf3a9e756c85d683695d4c3a242861cabe982
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2026-02-12 12:43:31 +0100

    refactor: add 2 additional harpoon select markers

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -39,6 +39,8 @@ return {
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end, { desc = "Harpoon file 2" })
         vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end, { desc = "Harpoon file 3" })
         vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end, { desc = "Harpoon file 4" })
+        vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end, { desc = "Harpoon file 5" })
+        vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end, { desc = "Harpoon file 6" })
         
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Harpoon prev" })
