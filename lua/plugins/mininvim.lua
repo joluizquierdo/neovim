76ad3abd09983374d7663ed3300302189c5bec8c
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2026-03-09 11:45:09 +0100

    refactor(mini.surround): add mappings

--- lua/plugins/mininvim.lua
+++ lua/plugins/mininvim.lua
@@ -1,4 +1,3 @@
--- Plugin Installation
 vim.pack.add({
     {
         src = "https://github.com/nvim-mini/mini.nvim",
@@ -6,15 +5,28 @@ vim.pack.add({
     }
 })
 
--- Enable plugins
-
 require('mini.ai').setup({})
+
+-- Surround
 require('mini.surround').setup({
     -- Number of lines within which surrounding is searched
     n_lines = 200,
+    mappings = {
+        add = 'ys',
+        delete = 'ds',
+        replace = 'cs'
+    }
 })
+
+-- Remap adding surrounding to Visual mode selection
+vim.keymap.del('x', 'ys')
+vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+-- Make special mapping for "add surrounding for line"
+vim.keymap.set('n', 'yss', 'ys_', { remap = true })
+
 require('mini.pairs').setup({})
-require('mini.comment').setup({}) -- improve comments experience
+require('mini.comment').setup({})    -- improve comments experience
 require('mini.trailspace').setup({}) -- show trailspaces
-require('mini.notify').setup({}) -- Show notifications in a single floating window
-require('mini.icons').setup({}) -- Show appealing icons
+require('mini.notify').setup({})     -- Show notifications in a single floating window
+require('mini.icons').setup({})      -- Show appealing icons
