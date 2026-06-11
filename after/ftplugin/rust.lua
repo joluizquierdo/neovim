ae0e167eb28eb298633a9090f53b48aae5191be7
Jolu Izquierdo <22649229+joluizquierdo@users.noreply.github.com>
2026-06-11 13:32:06 +0200

    refactor: change default expacing from 4 to 2

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -2,10 +2,10 @@
 vim.opt_local.expandtab = true
 
 -- Number of spaces a <Tab> counts for
-vim.opt_local.tabstop = 2
+vim.opt_local.tabstop = 4
 
 -- Number of spaces to use for each step of (auto)indent
-vim.opt_local.shiftwidth = 2
+vim.opt_local.shiftwidth = 4
 
 -- Number of spaces a <Tab> counts for while performing editing operations
-vim.opt_local.softtabstop = 2
+vim.opt_local.softtabstop = 4
